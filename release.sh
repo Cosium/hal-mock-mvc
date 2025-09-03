7c171b475b1ec8ed6ade2e6a503d309d62a3b287
Réda Housni Alaoui <reda-alaoui@hey.com>
2025-09-03 11:52:52 +0200

    Fix release script

--- release.sh
+++ release.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-env | grep MAVEN_GPG_PASSPHRASE > /dev/null
-if [ $? -eq 1 ]; then
- echo "Environment variable 'MAVEN_GPG_PASSPHRASE' is missing. Set it to empty if there is no passphrase."
- exit 1
-fi
+# Prime gpg-agent caches. See https://maven.apache.org/plugins/maven-gpg-plugin/usage.html
+echo "test" | gpg --clearsign > /dev/null
 ./mvnw --batch-mode clean release:prepare release:perform && git push && git push --tags
