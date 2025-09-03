7e52ba1c3c39b325b8435a58f68e97e9d361cce0
Réda Housni Alaoui <reda-alaoui@hey.com>
2025-09-03 11:47:42 +0200

    Upgrade maven-oss

--- release.sh
+++ release.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+env | grep MAVEN_GPG_PASSPHRASE > /dev/null
+if [ $? -eq 1 ]; then
+ echo "Environment variable 'MAVEN_GPG_PASSPHRASE' is missing. Set it to empty if there is no passphrase."
+ exit 1
+fi
 ./mvnw --batch-mode clean release:prepare release:perform && git push && git push --tags
